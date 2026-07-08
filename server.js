db85ddc88a2db2e518194ef7d21338a6df0327a6
Lidia Addisu <lidiaddisu21@gmail.com>
2026-07-08 05:55:55 -0700

    Implement user login and JWT authentication

--- server.js
+++ server.js
@@ -7,7 +7,7 @@ const authRoutes = require("./routes/authRoutes");
 const reportRoutes = require("./routes/reportRoutes");
 const app = express();
 
-const PORT = process.env.PORT || 3000;
+const PORT = 3000;
 
 // Middleware
 app.use(express.json());
